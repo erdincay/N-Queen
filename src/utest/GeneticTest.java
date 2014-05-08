194930bf8c6542b7f0392c706c53ca012b333c88
Kern <kding@whpm.com>
2014-05-08 11:28:40 -0800

    hill climbing debug

---
+++ src/utest/GeneticTest.java
@@ -0,0 +1,23 @@
+package utest;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GeneticTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testRun() throws Exception {
+
+    }
+}
